282b5703efe21ec29ad665400a9a73e2e938f342
YongKai Hong <67231152+hierha@users.noreply.github.com>
2022-08-09 19:07:01 +0800

    Update 1.2.3merge1.2.1 and 1.2.2.py

--- code/1.2.3merge1.2.1 and 1.2.2.py
+++ code/1.2.3merge1.2.1 and 1.2.2.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pandas as pd
 
-data_1 = pd.read_csv(r'E:\Project\PINet\data_set\database\STRING\processed\STRING to UniPort(STRING).txt',sep='\t')
-data_2 = pd.read_csv(r'E:\Project\PINet\data_set\database\STRING\processed\UniPort to KEGG(STRING).txt',sep='\t')
+#读取文件，形成dataFrame格式
+data_1 = pd.read_csv(r'E:\Project\PINet\data_set\database\STRING\processed\STRING to UniPort(STRING).csv',sep='\t')
+data_2 = pd.read_csv(r'E:\Project\PINet\data_set\database\STRING\processed\UniPort to KEGG(STRING).csv',sep='\t')
 
-data_3 = pd.merge(data_1 , data_2, how = 'inner',left_on ='To', right_on='From')
-data_4 = data_3[['From_x','To_y']]
-data_4.columns=['STRING_ID','KEGG_ID']
+#合并数据
+data_3 = pd.merge(data_1 , data_2, how = 'inner',on ='UniProt_ID',)
 
-data_4.to_csv(r'E:\Project\PINet\data_set\database\STRING\processed\STRING to KEGG(STRING).csv', sep='\t', index =None)
 
+
+#储存数据
+data_3.to_csv(r'E:\Project\PINet\data_set\database\STRING\processed\STRING to KEGG(STRING).csv', sep='\t', index =None)
